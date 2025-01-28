b18e832a41f6cdc307af5387ec620db4852bdbd7
AnthonyLugoD <anthonylugodiaz.iutso@gmail.com>
2025-01-27 21:29:23 -0400

    actualizacion de pantalla pedidos

--- app/src/pages/PatallaPedido/Widgets.js
+++ app/src/pages/PatallaPedido/Widgets.js
@@ -11,16 +11,21 @@ export function Producto(){
 
         <div className="frameProducto">
 
-            <img className="imgComida" src="https://static.vecteezy.com/system/resources/thumbnails/018/128/189/small/schezwan-noodles-or-szechuan-vegetable-png.png"></img>
+            <div className="productoInformacion">
+
+                <img className="imgComida" src="https://static.vecteezy.com/system/resources/thumbnails/018/128/189/small/schezwan-noodles-or-szechuan-vegetable-png.png"></img>
 
 
-            <div className = "descripcion">
-                <p id="productoTitulo">Titulo</p>
-                <p id="productoPrecio">Precio: 20$</p>
-                <p id="prductoDisponiblidad">Disponibilidad : Dispobible</p>
+                <div className = "descripcion">
+                    <p id="productoNombre">Nombre del producto</p>
+                    <p id="productoPrecio">20$</p>
+                    <p id="prductoDisponiblidad"> Disponible</p>
+
+                    
+                </div>
             </div>
 
-            <button className="btnAgregarPoducto">
+            <button id="btnAgregarPoducto">
                 +
             </button>
 
@@ -28,5 +33,60 @@ export function Producto(){
 
     )
 
+
+}
+
+export function Pedido(){
+
+    return (
+
+        <div className="framePedidoWidget">
+
+            <button className="btnSumarRestar" id="btnRestarProducto">
+
+                -
+
+            </button>
+
+            <p className="pedidoNombre">Nombre de123123123213l producto</p>
+            <p className="pedidoCantidad">3x</p>
+            <div className="pedidoTotal"><p className="pedidoTotalTexto">60$</p></div>
+
+
+            <button className="btnSumarRestar" id="btnSumarProducto">
+
+                +
+
+            </button>
+
+        </div>
+
+    )
+
+}
+
+export function WidgetNota(){
+
+    return (
+
+        <div className="mainNotaWidget">
+
+            <div className="frameTituloNota">
+                <p className="notaTitulo">Nota</p>
+                <button id="btneliminarNota">
+
+                    x
+
+                </button>
+                
+            </div>
+            
+            <p className="notaContenido">*La nota deberia tener un maximo de caracteres 1232 13123213213213  </p>
+         
+
+
+        </div>
+
+    )
 
 }
