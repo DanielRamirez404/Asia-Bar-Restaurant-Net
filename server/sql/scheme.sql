91f939072a2ae30f65a7eca7f68ef301f63b01ce
DanielRamirez404 <danielramirezabou404@gmail.com>
2025-02-12 22:26:34 -0400

    Update the database scheme

--- server/sql/scheme.sql
+++ server/sql/scheme.sql
@@ -3,9 +3,47 @@ CREATE DATABASE AsiaBarRestaurant;
 USE AsiaBarRestaurant;
 
 CREATE TABLE Users (
-    Username VARCHAR(50) PRIMARY KEY NOT NULL,
-    Password VARCHAR(50) NOT NULL
+    Username VARCHAR(25) PRIMARY KEY NOT NULL,
+    Password VARCHAR(100) NOT NULL
 );
 
-INSERT INTO Users VALUES ("admin", "admin");
-INSERT INTO Users VALUES ("user", "user");
+CREATE TABLE Clients (
+    IdDocument VARCHAR(20) PRIMARY KEY NOT NULL,
+    Name VARCHAR(50) NOT NULL,
+    Address VARCHAR(25) NOT NULL,
+    Phone VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Foods (
+    Name VARCHAR(50) PRIMARY KEY NOT NULL,
+    Availability INT DEFAULT TRUE NOT NULL,
+    Price FLOAT NOT NULL,
+    Category VARCHAR(50) NOT NULL,
+    Description VARCHAR(100)
+);
+
+CREATE TABLE MainDish (
+    FoodName VARCHAR(50),
+    FOREIGN KEY (FoodName) REFERENCES Foods(Name) 
+    -- Image BLOB NOT NULL (leaving this here just in case we need it)
+);
+
+CREATE TABLE SideDish (
+    FoodName VARCHAR(50),
+    FOREIGN KEY (FoodName) REFERENCES Foods(Name)
+);
+
+CREATE TABLE Product (
+    FoodName VARCHAR(50),
+    FOREIGN KEY (FoodName) REFERENCES Foods(Name), 
+    Provider VARCHAR(50) NOT NULL,
+    Quantity INT NOT NULL
+);
+
+CREATE TABLE Sales (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    ClientIdDocoment VARCHAR(20),
+    FOREIGN KEY (ClientIdDocoment) REFERENCES Clients(IdDocument),
+    Type VARCHAR(20) NOT NULL,
+    Total FLOAT NOT NULL
+);
