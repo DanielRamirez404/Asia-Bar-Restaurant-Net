ca495e9666c71be6065f1cc42d257006aee1b7a9
DanielRamirez404 <danielramirezabou404@gmail.com>
2025-02-23 01:11:09 -0400

    Fix typo on route

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,10 +1,9 @@
 import { Router } from 'express';
 import { signup, login } from '../controllers/auth.controllers.js';
 
-
 const router = Router();
 
-router.post('/sing-up', signup);
+router.post('/sign-up', signup);
 router.post('/login', login);
 
 export default router;
