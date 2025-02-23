1df803aa9efa0b3d1f0bea3b801bad71bd031ee0
DanielRamirez404 <danielramirezabou404@gmail.com>
2025-02-23 01:12:08 -0400

    Implement handleQueryExecution on both test endpoints

--- server/src/controllers/auth.controllers.js
+++ server/src/controllers/auth.controllers.js
@@ -1,4 +1,32 @@
-export const signup = (req, res) => res.send("signing up");
+import mysql2 from 'mysql2/promise';
+import handleQueryExecution from './handleQueryExecution.js';
 
-export const login = (req, res) => res.send("logging in");
+export const signup = async (req, res) => {
+    const { username, type, password } = req.body;
+        
+    handleQueryExecution(res, async (db) => {
+        const [results, fields] = await db.execute(
+            'INSERT INTO Users VALUES (?, ?, ?)', 
+            [username, type, password]
+        )
+
+        res.status(200).send('Signed up successfully');
+    });
+};
+
+export const login = (req, res) => {
+    const { username, password } = req.body;
+
+    handleQueryExecution(res, async (db) => {
+        const [results, fields] = await db.execute(
+            'SELECT * FROM Users WHERE username = ? AND password = ?', 
+            [username, password]
+        )
+
+        if (results.length < 1)
+            res.status(400).send('Username or password not valid');
+        else
+            res.status(200).send('Logged in successfully')
+    });
+}
 
