bf926dfaa0a6ecc651b54a7bea46fb7884680946
DanielRamirez404 <danielramirezabou404@gmail.com>
2025-03-16 15:37:55 -0400

    Add table.js

---
+++ server/src/libs/table.js
@@ -0,0 +1,27 @@
+class Table {
+    constructor(name, idName, isFood = false, isAdminOnly = false) {
+        this.name = name;
+        this.idName = idName;
+        this.isFood = isFood;
+        this.isAdminOnly = isAdminOnly;
+    }
+
+    getUrlName() {
+        const capitalLetters = this.name.match(/[A-Z]/g);
+        const numberOfCapitalLetters = capitalLetters ? capitalLetters.length : 0;
+
+        if (numberOfCapitalLetters < 2)
+            return this.name;
+
+        let urlName = this.name.toLowerCase().charAt(0);
+
+        for (let i = 1; i < this.name.length; i++) {
+            const character = name.charAt(i); 
+            urlName += (character == character.toLowerCase()) ? character : "-" + character.toLowerCase();
+        }
+
+        return urlName;
+    }
+}
+
+export default Table;
