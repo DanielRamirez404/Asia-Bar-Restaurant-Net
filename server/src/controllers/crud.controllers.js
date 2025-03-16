762232576ecedc19555ee677c1ad2ca8e0a5be4c
DanielRamirez404 <danielramirezabou404@gmail.com>
2025-03-15 21:39:58 -0400

    Automatize several get-all endpoints

--- server/src/controllers/crud.controllers.js
+++ server/src/controllers/crud.controllers.js
@@ -1,6 +1,17 @@
 import handleQueryExecution from '../libs/handleQueryExecution.js';
 import { sendAllRegistersFrom } from '../libs/crudOperations.js';
 
-export const getAllUsers = (req, res) => {
-    sendAllRegistersFrom(res, "Users"); 
-}
+const independentTables= [
+    "Users",
+    "Clients",
+    "Sales"
+];
+
+export const getEndpoints = {};
+
+independentTables.forEach(table => {
+    getEndpoints[table] = function (req, res) {
+        sendAllRegistersFrom(res, table); 
+    };
+});
+
--- server/src/routes/crud.routes.js
+++ server/src/routes/crud.routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
-import { getAllUsers } from '../controllers/crud.controllers.js';
+import { getEndpoints } from '../controllers/crud.controllers.js';
 import { validateToken } from '../middlewares/validateToken.js';
 
 const crudRouter = Router();
 
 crudRouter.use(validateToken);
 
-crudRouter.get('/get-users', getAllUsers);
+Object.entries(getEndpoints).forEach( ( [table, endpointFunction] ) => {
+    crudRouter.get(`/${ table.toLowerCase() }/get-all`, endpointFunction); 
+});
 
 export default crudRouter;
