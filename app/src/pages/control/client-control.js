0b8eca879eb3b4b13f0011c23a6c0ae14e6aa83f
DanielRamirez404 <danielramirezabou404@gmail.com>
2025-03-30 17:40:41 -0400

    Connect clients table to the backend

--- app/src/pages/control/client-control.js
+++ app/src/pages/control/client-control.js
@@ -1,16 +1,10 @@
 import TablePage from "../reusables/tablePage";
 
-const fields = [ "Nombre", "Identificación", "Dirección", "Teléfono" ];
-
-const data = [ 
-    ["David Rodriguez", "V-27.121.122", "Barcelona", "0414-1112222"],
-    ["Luisa Jiménez", "V-11.121.122", "PLC", "0424-1112222"],
-    ["Panificadora El Gallo", "J-14386741-213", "Lechería", "0412-1234567"],
-];
+const fields = ["Identificación", "Nombre", "Dirección", "Teléfono" ];
 
 const ClientControl = () => {
     return(
-        <TablePage title={ "Control de Clientes" } fields={ fields } data={ data }/>
+        <TablePage title={ "Control de Clientes" } fields={ fields } tableName="clients"/>
     );
 }
 
--- app/src/pages/control/user-control.js
+++ app/src/pages/control/user-control.js
@@ -2,12 +2,6 @@ import TablePage from "../reusables/tablePage";
 
 const fields = [ "Usuario", "Tipo", "Contraseña" ];
 
-const data = [ 
-    ["David", "Cocinero", "david123"],
-    ["MartinezJ", "Cajero", "jesusD4Silva"],
-    ["Rodrigo", "Admin", "1234567890"]
-];
-
 const UserControl = () => {
     return(
         <TablePage title={ "Control de Usuarios" } fields={ fields } tableName="users" />
