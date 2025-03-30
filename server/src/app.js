4a92083b9b9191aba2da82399f2d25f3cf4adc93
DanielRamirez404 <danielramirezabou404@gmail.com>
2025-03-30 16:25:59 -0400

    Update cors and cookies for them to be accessible

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,18 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:3000', 
+  'http://127.0.0.1:3000',
+];
+
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(CookieParser());
