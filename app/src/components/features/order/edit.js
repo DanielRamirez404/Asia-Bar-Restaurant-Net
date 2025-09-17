cd782e0931d52a033a6a3912b22c9e5bf59cbda2
DanielRamirez404 <danielramirezabou404@gmail.com>
2025-09-17 17:50:51 -0400

    Add the EditProductsSection component and styles

---
+++ app/src/components/features/order/edit.js
@@ -0,0 +1,63 @@
+import './edit.css';
+
+function ProductInput({name, value, onChange, type = "text"}) {
+    const isNumber = type === "number" || type === "int";
+    const min = !isNumber ? null : type === "int" ? 1 : 0.01;
+
+    return(
+        <div className="product-input-container">
+            <p className="product-input-title">{name}</p>
+            <input
+                className="product-input"
+                type={type === "int" ? "number" : type}
+                min={min}
+                step={min}
+                value={value}
+                onChange={(e) => onChange(e.target.value)}
+                placeholder={name}
+                required
+            >
+            </input>
+        </div>
+    );
+}
+
+function ProductCard({ name, cost, quantity }) {
+    return(
+        <div className="product-card">
+            <ProductInput name={"Nombre"} value={name.value} onChange={name.setter} />
+            <ProductInput name={"Costo ($)"} type="number" value={cost.value} onChange={cost.setter} />
+            <ProductInput name={"Cant."} type="int" value={quantity.value} onChange={quantity.setter} />
+        </div>
+    );
+}
+
+export function EditProductsSection({ products }) {
+    const getFieldSetter = (product, i) => (newValue) => {
+        const newProduct = product.value || "";
+        newProduct[i] = newValue;
+        product.setter(newProduct);
+    };
+
+    const fields = ["name", "cost", "quantity"];
+
+    return (
+        <div className="edit-products-container">
+            {
+                products.map((product) => {
+
+                    const getFieldProp = (field, i) => ({ 
+                        value: product.value[i], 
+                        setter: getFieldSetter(product, i) 
+                    });
+
+                    const props = fields.reduce((object, field, i) => ({...object, [field]: getFieldProp(field, i)}), {});
+
+                    return(
+                        <ProductCard {...props} />
+                    );
+                })
+            }
+        </div>
+    );
+}
